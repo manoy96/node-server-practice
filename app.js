1447ddbce447d9dff7472977b40ffa6fc91bb7b2
Manuel Espinoza <mfewcar@gmail.com>
2019-01-31 13:12:21 -0700

    router.Get, Router.post, and other product controller stuff

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+const dev_db_url = 'mongodb+srv://someuser:ABCD1234@product-cluster-diz8n.gcp.mongodb.net/product'
+const mongoDB = process.env.MONGODB_URI || dev_db_url
+mongoose.connect(mongoDB, {useNewUrlParser: true})
+mongoose.Promise = global.Promise
+const db = mongoose.connection
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 const product = require('./routes/product.route')
 
 const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
 
 app.use('/products', product)
 
--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -3,3 +3,16 @@ const Product = require('../models/product.model')
 exports.test = (req, res, next) => {
     res.send('Greetings from the test controller!')
 }
+
+exports.product_create = (req, res, next) => {
+    let product = new Product({
+        name: req.body.name,
+        price: req.body.price,
+    })
+       product.save((err) => {
+           if(err) {
+               return next(err)
+           }
+           HTMLTableRowElement.send('Product created successfully!')
+       }) 
+}
