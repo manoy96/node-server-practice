f0bb58c3d94882748427e9cf8855c2b9c0942234
Manuel Espinoza <mfewcar@gmail.com>
2019-02-14 22:36:47 -0700

    attempting to integrate nasa api

--- seed/populate.js
+++ seed/populate.js
@@ -30,28 +30,23 @@
 
 
 
-
-
-
-
-
 const fetch = require('node-fetch')
 
-fetch('https://pokeapi.co/api/v2/pokemon?limit=25')
+fetch('https://api.nasa.gov/planetary/apod?api_key=diU3YOSoUyfoY6PSV2zqJn9bEZ1geyaJ7J61cDy1&start_date=2019-01-20&end_date=2019-02-14')
   .then(res => res.json())
   .then(myJson => {
-    const pokeArray = myJson.results
-    pokeArray.forEach(pokemon => {
-      let purl = pokemon.url
-      fetch(purl)
+    const nasaArray = myJson.copyright
+    nasaArray.forEach(apod => {
+      let nurl = apod.url
+      fetch(nurl)
           .then(result => result.json())
-          .then(fullPokemon => {
+          .then(fullApod => {
             return newPokemon = {
-              height: fullPokemon.height,
-              weight: fullPokemon.weight,
-              name: fullPokemon.name,
+              height: fullApod.height,
+              weight: fullApod.weight,
+              name: fullApod.name,
               url: purl,
-              sprite: fullPokemon.sprites.front_shiny,
+              sprite: fullApod.sprites.front_shiny,
             }
           })
       .then(newOne => {
@@ -62,6 +57,37 @@ fetch('https://pokeapi.co/api/v2/pokemon?limit=25')
     })
   })
 })
+
+
+
+
+// const fetch = require('node-fetch')
+
+// fetch('https://pokeapi.co/api/v2/pokemon?limit=25')
+//   .then(res => res.json())
+//   .then(myJson => {
+//     const pokeArray = myJson.results
+//     pokeArray.forEach(pokemon => {
+//       let purl = pokemon.url
+//       fetch(purl)
+//           .then(result => result.json())
+//           .then(fullPokemon => {
+//             return newPokemon = {
+//               height: fullPokemon.height,
+//               weight: fullPokemon.weight,
+//               name: fullPokemon.name,
+//               url: purl,
+//               sprite: fullPokemon.sprites.front_shiny,
+//             }
+//           })
+//       .then(newOne => {
+//         console.log(newOne)
+//         postData(`http://localhost:5775/pokemon/pokemon`, newOne)
+//         .then(data => console.log(data))
+//         .catch(error => console.error(error))
+//     })
+//   })
+// })
 
 const postData = (url = ``, data = {}) => {
   return fetch(url, {
