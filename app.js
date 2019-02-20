cf286fb2a53b0e0b6299f68ded5dc0f25e81208d
Manuel Espinoza <mfewcar@gmail.com>
2019-02-20 15:13:11 -0700

    heroku stuff

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ app.use((req, res, next) => {
     next()
 })
 
-const port = 5775
+const port = (process.env.PORT || 5000)
 
 app.listen(port, () => {
     console.log(`Server is up and running on port ${port}`)
